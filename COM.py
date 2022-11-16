d316dc5a30bd247bbc073d113141d217c67eaff9
Stylianos-Grigoriadis <stylgrig@yahoo.gr>
2022-11-16 16:32:18 +0200

    sdf

--- COM.py
+++ COM.py
@@ -9,7 +9,7 @@ from tkinter import filedialog
 #                                       # title = "Title",
 #                                       filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
 
-df_COM = pd.read_csv('Big Movement with first cut.csv',
+df_COM = pd.read_csv('C:\Python_projects\Margin of Stability\data\Big Movement with first cut.csv',
                      delimiter=',',
                      decimal='.',
                      thousands=',',
@@ -21,23 +21,95 @@ ymax = (1280*100)/big_movement_pixel_of_100cm
 xmax = (720*100)/big_movement_pixel_of_100cm
 print('ymax = ' +str(ymax))
 print('xmax = ' +str(xmax))
-plot = plt.plot(df_COM['right_wrist_x'],df_COM['right_wrist_y'])
+#plot = plt.plot(df_COM['right_wrist_x'],df_COM['right_wrist_y'])
 # plot = plt.plot(df_COM['right_wrist_y'])
-plt.show()
+#plt.show()
 
-def COM_segments(x_proximal,x_distal,y_proximal,y_distal,percentage_of_COM_segment):
+def COM_segments(x_proximal,x_distal,y_proximal,y_distal,distance_of_CoM_of_each_segment_from_the_proximal,mi):\
+    #mi = εκατοστιαια ποσοτητα μαζας καθε μελους
+    #print(type(x_proximal))
+    #if x_proximal[0] == 'left_heel_x':
+       # print(True)
+    #print(x_proximal)
     x = []
     y = []
     for i in range(len(x_proximal)):
-        x_data = (x_proximal[i] + (percentage_of_COM_segment) * (x_distal[i] - x_proximal[i])) * xmax
-        x.append(x_data)
-        y_data = (y_proximal[i] + (percentage_of_COM_segment) * (y_distal[i] - y_proximal[i])) * ymax
-        y.append(y_data)
+        x_data = (x_proximal[i] + (distance_of_CoM_of_each_segment_from_the_proximal) * (x_distal[i] - x_proximal[i])) * xmax
+        x.append(x_data*mi)
+        y_data = (y_proximal[i] + (distance_of_CoM_of_each_segment_from_the_proximal) * (y_distal[i] - y_proximal[i])) * ymax
+        y.append(y_data*mi)
     return x,y
-#βραχιωνας
-right_forarm = COM_segments(df_COM['right_shoulder_x'],df_COM['right_elbow_x'],df_COM['right_shoulder_y'],df_COM['right_elbow_y'],3.2)
-print(type(right_forarm))
-print(right_forarm)
-print(right_forarm[1])
-# vasili
+#find the coordinates of each segment
+#use of πινακας 5.8 kollias
+#right_upperarm
+right_upperarm = COM_segments(df_COM['right_shoulder_x'],df_COM['right_elbow_x'],df_COM['right_shoulder_y'],df_COM['right_elbow_y'],0.436,0.027)
+#print(type(right_upperarm))
+#print(right_upperarm)
+#print(right_upperarm[1])
+#left_upperarm
+left_upperarm = COM_segments(df_COM['left_shoulder_x'],df_COM['left_elbow_x'],df_COM['left_shoulder_y'],df_COM['left_elbow_y'],0.436,0.027)
+#head_and_neck
+#head_and_neck = COM_segments(df_COM['left_shoulder_x'],df_COM['left_elbow_x'],df_COM['left_shoulder_y'],df_COM['left_elbow_y'],2.8)
+#right_forarm
+right_forarm = COM_segments(df_COM['right_elbow_x'],df_COM['right_index_x'],df_COM['right_elbow_y'],df_COM['right_index_y'],0.677,0.023)
+#left_forarm
+left_forarm = COM_segments(df_COM['left_elbow_x'],df_COM['left_index_x'],df_COM['left_elbow_y'],df_COM['left_index_y'],0.677,0.023)
+# #right_palm
+# right_palm = COM_segments(df_COM['right_wrist_x'],df_COM['right_index_x'],df_COM['right_wrist_y'],df_COM['right_index_y'],0.6)
+# #left_palm
+# left_palm = COM_segments(df_COM['left_wrist_x'],df_COM['left_index_x'],df_COM['left_wrist_y'],df_COM['left_index_y'],0.6)
+#right_thigh
+right_thigh = COM_segments(df_COM['right_hip_x'],df_COM['right_knee_x'],df_COM['right_hip_y'],df_COM['right_knee_y'],0.433,0.102)
+#left_thigh
+left_thigh = COM_segments(df_COM['left_hip_x'],df_COM['left_knee_x'],df_COM['left_hip_y'],df_COM['left_knee_y'],0.433,0.102)
+#right_shank
+right_shank = COM_segments(df_COM['right_knee_x'],df_COM['right_ankle_x'],df_COM['right_knee_y'],df_COM['right_ankle_y'],0.433,0.046)
+#left_shank
+left_shank = COM_segments(df_COM['left_knee_x'],df_COM['left_ankle_x'],df_COM['left_knee_y'],df_COM['left_ankle_y'],0.433,0.046)
+#right_foot
+right_foot = COM_segments(df_COM['right_heel_x'],df_COM['right_foot_index_x'],df_COM['right_heel_y'],df_COM['right_foot_index_y'],0.149,0.015)
+#left_foot
+left_foot = COM_segments(df_COM['left_heel_x'],df_COM['left_foot_index_x'],df_COM['left_heel_y'],df_COM['left_foot_index_y'],0.149,0.015)
+#to find the trunk we need to take the average position of the shoulders and the hips and assume that
+#the trunk is the distance between those
+#proximal from shoulders
+#distal from hips
+proximal_trunk_x = []
+proximal_trunk_y = []
+distal_trunk_x = []
+distal_trunk_y = []
+for i in range(len(df_COM['right_shoulder_x'])):
+
+    #print(i)
+    proximal_x = ((df_COM['right_shoulder_x'][i]+df_COM['left_shoulder_x'][i])/2) * xmax
+    #print(type(proximal_x))
+    proximal_trunk_x.append(proximal_x)
+    proximal_y = ((df_COM['right_shoulder_y'][i] + df_COM['left_shoulder_y'][i]) / 2) * ymax
+    proximal_trunk_y.append(proximal_y)
+    distal_x = ((df_COM['right_hip_x'][i]+df_COM['left_hip_x'][i])/2) * xmax
+    distal_trunk_x.append(distal_x)
+    distal_y = ((df_COM['right_hip_y'][i] + df_COM['left_hip_y'][i]) / 2) * ymax
+    distal_trunk_y.append(distal_y)
+print(proximal_trunk_x)
+trunk = COM_segments(proximal_trunk_x,distal_trunk_x,proximal_trunk_y,distal_trunk_y,0.500,0.495)
+print(type(trunk))
+#To find the head and neck coordinates we will add 12.5 cm to each of the nose data so that we can find the
+#highest position of the head at each time and use the proximal trunk positions (x,y) as proximal position of
+#the neck
+#first we make a list with the nose data so that we can turn them form percentages to cm and then add 12.5
+head_neck_distal_x = []
+head_neck_distal_y = []
+
+for i in range(len(df_COM['nose_x'])):
+    x = (df_COM['nose_x'][i] * xmax) + 12.5
+    head_neck_distal_x.append(x)
+    y = (df_COM['nose_y'][i] * ymax) + 12.5
+    head_neck_distal_y.append(y)
+print(head_neck_distal_x)
+head_neck = COM_segments(proximal_trunk_x,head_neck_distal_x,proximal_trunk_y,head_neck_distal_y,0.567,0.079)
+
+#To find the coordinates (x,y) of the CoM
+print(len(head_neck))
+#for i in range(len(head_neck)):
+
 
