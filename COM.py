9ef3bd00fe621bddd85f29dc8428c0b7e3899680
Stylianos-Grigoriadis <stylgrig@yahoo.gr>
2022-11-21 12:55:06 +0200

    Interpolation + CoP

--- COM.py
+++ COM.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
 from tkinter import filedialog
+from matplotlib.widgets import SpanSelector
 
 
 # filename = filedialog.askopenfilename(initialdir="C:\\",
@@ -8,17 +11,26 @@ from tkinter import filedialog
 #                                       title="Select CSV File",
 #                                       # title = "Title",
 #                                       filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
+import lib
 
-df_COM = pd.read_csv('C:\Python_projects\Margin of Stability\data\Big Movement with first cut.csv',
+df_COM = pd.read_csv('Big Movement with first cut video.csv',
                      delimiter=',',
                      decimal='.',
                      thousands=',',
                      # skiprows=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
                      )
-print(df_COM)
-big_movement_pixel_of_100cm = 413 #pixel
-ymax = (1280*100)/big_movement_pixel_of_100cm
-xmax = (720*100)/big_movement_pixel_of_100cm
+#print(df_COM)
+column_names = list(df_COM.columns.values)
+print(column_names)
+column_names.remove('timestamp')
+column_names.remove('angle_left')
+column_names.remove('angle_right')
+print(column_names)
+
+
+big_movement_pixel_of_1m = 413 #pixel
+ymax = (1280*1)/big_movement_pixel_of_1m
+xmax = (720*1)/big_movement_pixel_of_1m
 print('ymax = ' +str(ymax))
 print('xmax = ' +str(xmax))
 #plot = plt.plot(df_COM['right_wrist_x'],df_COM['right_wrist_y'])
@@ -38,6 +50,7 @@ def COM_segments(x_proximal,x_distal,y_proximal,y_distal,distance_of_CoM_of_each
         #x_data = (x_proximal[i] + (distance_of_CoM_of_each_segment_from_the_proximal) * (x_distal[i] - x_proximal[i])) * xmax
         x.append(x_data*mi)
         y_data = (y_proximal[i] - (y_proximal[i] - y_distal[i]) * distance_of_CoM_of_each_segment_from_the_proximal) * ymax
+        y_data = abs(ymax - y_data)
         #y_data = (y_proximal[i] + (distance_of_CoM_of_each_segment_from_the_proximal) * (y_distal[i] - y_proximal[i])) * ymax
         y.append(y_data*mi)
     return x,y
@@ -103,7 +116,7 @@ head_neck_distal_x = []
 head_neck_distal_y = []
 
 for i in range(len(df_COM['nose_x'])):
-    x = (df_COM['nose_x'][i] * xmax) + 12.5
+    x = (df_COM['nose_x'][i] * xmax)
     head_neck_distal_x.append(x)
     y = (df_COM['nose_y'][i] * ymax) + 12.5
     head_neck_distal_y.append(y)
@@ -123,9 +136,109 @@ for i in range(len(head_neck[0])):
     COM_x.append(COMx)
     COM_y.append(COMy)
 
-print(COM_x)
-print(COM_y)
-plot = plt.plot(COM_x,COM_y)
-#plt.legend('Center of Mass')
+# print(COM_x)
+# print(COM_y)
+# #plot = plt.plot(COM_x,COM_y)
+# fig,(ax1,ax2,ax3)=plt.subplots(3)
+# ax1.plot(COM_x)
+# ax2.plot(COM_y)
+# ax3.plot(COM_x,COM_y)
+# #plt.legend('Center of Mass')
+# plt.show()
+
+df_force = pd.read_csv('Big Movement force.csv',
+                     delimiter=',',
+                     decimal='.',
+                     thousands=',',
+                     skiprows=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+                     index_col= False
+                     )
+print(df_force.columns)
+df_force.rename(columns = {'Time (s)':'Time (s)',
+                              'CHANNEL_1' : 'CHANNEL_1L',
+                              'CHANNEL_2' : 'CHANNEL_2L',
+                              'CHANNEL_3' : 'CHANNEL_3L',
+                              'CHANNEL_4' : 'CHANNEL_4L',
+                              'Unnamed: 5': 'Unnamed: 5',
+                              'CHANNEL_1.1' : 'CHANNEL_1R',
+                              'CHANNEL_2.1' : 'CHANNEL_2R',
+                              'CHANNEL_3.1' : 'CHANNEL_3R',
+                              'CHANNEL_4.1' : 'CHANNEL_4R'
+                           }, inplace = True)
+print(df_force.columns)
+# plt.plot(df_force['CHANNEL_1.1'], df_force['CHANNEL_1'])
+# plt.show()
+# fig,(ax1,ax2,ax3,ax4)=plt.subplots(4)
+# ax1.plot(df_force['CHANNEL_1.1'])
+# ax2.plot(df_force['CHANNEL_2.1'])
+# ax3.plot(df_force['CHANNEL_3.1'])
+# ax4.plot(df_force['CHANNEL_4.1'])
+#
+# #plt.legend('Center of Mass')
+# plt.show()
+list_of_right_platform = []
+
+for i in range(len(df_force['CHANNEL_1R'])):
+    list = df_force['CHANNEL_1R'][i] + df_force['CHANNEL_2R'][i] + df_force['CHANNEL_3R'][i] + df_force['CHANNEL_4R'][i]
+    list_of_right_platform.append(list)
+    print(list_of_right_platform[i])
+
+fig, (ax1) = plt.subplots(1)
+
+ax1.plot(list_of_right_platform)
+ax1.set_title('Press left mouse button and drag '
+              'to select a region in the top graph')
+
+def onselect(xmin, xmax):
+    max_y = list_of_right_platform[int(xmin)]
+    global max_y_index
+    for l in range(int(xmin),int(xmax)):
+        if list_of_right_platform[l] > max_y:
+            max_y = list_of_right_platform[l]
+            max_y_index = l
+
+    print(max_y)
+    print(max_y_index)
+
+span = SpanSelector(
+    ax1,
+    onselect,
+    "horizontal",
+    useblit=True,
+    props=dict(alpha=0.5, facecolor="tab:red"),
+    interactive=True,
+    drag_from_anywhere=True
+)
 plt.show()
+print(df_force)
+
+print(df_force)
+print(len(COM_y))
+df_force = df_force[max_y_index:].reset_index()
+del df_force["index"]
+print(df_force)
+list_of_right_platform = list_of_right_platform[max_y_index:]
+COM_y = COM_y[:797]
+COM_x = COM_x[:797]
+
+step = len(COM_y)/len(list_of_right_platform)
+COM_y = lib.Linear_Interpolation(COM_y,step,plus=0)
+COM_x = lib.Linear_Interpolation(COM_x,step,plus=0)
+
+fig,ax=plt.subplots()
+ax.plot(list_of_right_platform, label = 'Force',color='blue')
+ax2 = ax.twinx()
+plt.plot(COM_x,label='CoM',color='red')
+plt.legend()
+plt.show()
+
+CoP = lib.compute_cop(31,26,df_force)
+
+fig,ax=plt.subplots()
+ax.plot(CoP[0], label = 'CoP',color='blue')
+ax2 = ax.twinx()
+plt.plot(COM_x,label='CoM',color='red')
+plt.legend()
+plt.show()
+print(CoP[0])
 
